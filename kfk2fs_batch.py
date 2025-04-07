ccdbe64829c63b076a6c51eb5f10f8c2ccf5bc49
aramis2008 <sec@y.ru>
2025-04-07 12:04:15 +0300

    improve

--- kfk2fs_batch.py
+++ kfk2fs_batch.py
@@ -1,7 +1,8 @@
+from time import sleep
+import datetime
 from pyspark.sql import SparkSession, DataFrame
 
-def proccess_batch(df: DataFrame, batch_id: int):
-    print(df.count(), batch_id)
+def compute_batch(df: DataFrame):
     (df
      .filter("topic = 'ark-topic-1'")
      .write
@@ -31,33 +32,29 @@ spark = (SparkSession
          .getOrCreate())
 spark.sparkContext.setLogLevel('WARN')
 
+topic_name_1 = 'ark-topic-1'
+topic_name_2 = 'ark-topic-2'
+topic_name_3 = 'ark-topic-3'
+
+# Если запросить оффсет меньше существующего, то при failOnDataLoss=False ошибка хоть и будет в логе, но приложение не прервется, данные которые выше все равно загрузятся
+# Если запросить оффсет больше существующего, то при failOnDataLoss=False будет ошибка которая прервет все
+startingOffsets = f"""{{"{topic_name_1}":{{"0":1000}},"{topic_name_2}":{{"0":1000}},"{topic_name_3}":{{"0":1700}} }}"""
+
 df = (spark
           .read
           .format('kafka')
           .option('kafka.bootstrap.servers', 'localhost:9092')
-          .option('subscribe', 'ark-topic-1,ark-topic-2,ark-topic-3')
-          .option("startingOffsets", "earliest")
+          .option('subscribe', f'{topic_name_1},{topic_name_2},{topic_name_3}')
+          .option("startingOffsets", startingOffsets) # "earliest"
+          .option("failOnDataLoss", False)
           .option('checkpointLocation', '/home/aramis2008/sparkstreamingFromKafka/checkpoint')
           .load())
 
-(df
-    .filter("topic = 'ark-topic-1'")
-    .write
-    .format('json')
-    .mode('append')
-    .option('encoding', 'UTF-8')
-    .save('/home/aramis2008/sparkstreamingFromKafka/outputStreaming2/topic-1'))
-(df
-    .filter("topic = 'ark-topic-2'")
-    .write
-    .format('json')
-    .mode('append')
-    .option('encoding', 'UTF-8')
-    .save('/home/aramis2008/sparkstreamingFromKafka/outputStreaming2/topic-2'))
-(df
-    .filter("topic = 'ark-topic-3'")
-    .write
-    .format('json')
-    .mode('append')
-    .option('encoding', 'UTF-8')
-    .save('/home/aramis2008/sparkstreamingFromKafka/outputStreaming2/topic-3'))
+
+print(datetime.datetime.now().time(), '1 batch start')
+compute_batch(df)
+print(datetime.datetime.now().time(), '1 batch finish')
+# sleep(10)
+# print(datetime.datetime.now().time(), '2 batch start')
+# compute_batch(df)
+# print(datetime.datetime.now().time(), '2 batch finish')
--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-from pyspark.sql import SparkSession
+topic_name_1 = 'ark-topic-1'
+topic_name_2 = 'ark-topic-2'
+topic_name_3 = 'ark-topic-3'
 
-columns = ["language","users_count"]
-data = [("Java", "20000"), ("Python", "100000"), ("Scala", "3000")]
-spark = (SparkSession
-         .builder
-         .appName('SparkByExamples')
-         .getOrCreate())
-df = spark.sparkContext.parallelize(data).toDF(columns)
-
-df.show()
+startingOffsets = f"""{{"{topic_name_1}":{{"0":370}},"{topic_name_2}":{{"0":220}},"{topic_name_3}":{{"0":1550}} }}"""
+print(startingOffsets)
